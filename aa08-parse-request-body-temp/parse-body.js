0dc7cd18af14d0a328fd8e83880ecbcbf4725df7
Alex Garbarino <alexiothebrave@gmail.com>
2024-06-22 13:28:23 -0500

    all passing

--- aa08-parse-request-body-temp/parse-body.js
+++ aa08-parse-request-body-temp/parse-body.js
@@ -1,25 +1,91 @@
 function firstStep(input) {
-  // Your code here 
+  //! input starts out as the original request body string.
+  //! seperate key and value pairs out of the string.
+  const kvPairArr = input.split('&')
+
+  return kvPairArr
 }
 
 function secondStep(input) {
-  // Your code here 
+  //! input is now the array created from the req body, each element is a string of the
+  //! key and value put -> "key=value"
+  //! seperate the keys and values from each element
+
+    return input.map(el => el.split('='))
+
 }
 
 function thirdStep(input) {
-  // Your code here 
+  //! input will be an array with sub arrays of each k-v pairs.
+  //! scan each value for a + and replace with a " " space
+  return input.map(([k, v]) => [k, v.replace(/\+/g, " ")])
+
+    // input.forEach(el => {
+    //   for (let i = 0; i < el.length; i++){
+    //     const str = el[i];
+    //     el[i] = str.replace("+", " ");
+    //   }
+    // })
+
+    // return input
+
 }
 
 function fourthStep(input) {
-  // Your code here 
+  //! input will be an array with sub arrays of k-v pairs, + replaced with space.
+  //! grab each value and run it through the decodeURIComponent function.
+  //! decode the percent encoded character
+  // console.log('TEST');
+  // input.forEach(el => {
+  //   for (let i = 0; i < el.length; i++){
+  //     const str = el[i];
+  //     if (str.includes('%')) {
+
+  //       el[i] = decodeURIComponent(str);
+
+  //     }
+  //   }
+  // })
+
+  // return input
+
+  return input.map(([k, v]) => [k, decodeURIComponent(v)])
 }
 
 function fifthStep(input) {
-  // Your code here 
+  //! input is an array of fully converted k-v pair strings.
+  //! iterate through each sub array and add the elements to an object where first el is
+  //! key and second el is a value
+
+  //! reduce our array into a single object, in the reduce function
+  //! destructure the el subArray to k,v then set the acc.k to have the value of v.
+
+
+  return input.reduce((acc, [k, v]) => {
+    acc[k] = v;
+    return acc
+  }, {})
 }
 
 function parseBody(str) {
-  // Your code here 
+
+
+  const splitString = firstStep(str);
+  // console.log(res);
+
+  const keyValArrays = secondStep(splitString);
+  // console.log(res);
+
+  const replacedPlus = thirdStep(keyValArrays);
+  // console.log(res)
+
+  const decodeValues = fourthStep(replacedPlus);
+  // console.log('fourth', res)
+
+
+  return fifthStep(decodeValues)
+  // console.log(res);
+
 }
 
 /******************************************************************************/
--- aa08-parse-request-body-temp/server.js
+++ aa08-parse-request-body-temp/server.js
@@ -1,11 +1,33 @@
 const { sendFormPage } = require("./routes");
 const { parseBody } = require("./parse-body");
+const http = require('http');
 let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
+server = http.createServer((req, res) => {
+  console.log(req.method, req.url);
+  let reqBody = '';
 
-// Your code here 
+  req.on('data', (data) => {
+    reqBody += data;
+  })
+  
+  req.on('end', () => {
+    //!parse the body here
+    console.log(reqBody);
+    if (reqBody) {
+        req.body = parseBody(reqBody)
+    }
+
+    sendFormPage(req, res)
+})
+})
+
+const port = 5000
+server.listen(port, () => {
+  console.log('server is listening on port', port)
+})
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
